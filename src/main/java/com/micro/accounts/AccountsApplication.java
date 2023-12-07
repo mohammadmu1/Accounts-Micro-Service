30ed919cf5d8e8c4de0462129fa6f6dc931cd2af
mohammadmu1 <mbn.1991@yahoo.com>
2023-12-08 02:34:02 +0300

    Docker File

--- src/main/java/com/micro/accounts/AccountsApplication.java
+++ src/main/java/com/micro/accounts/AccountsApplication.java
@@ -1,5 +1,8 @@
 package com.micro.accounts;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +10,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 
 @EnableJpaAuditing(auditorAwareRef = "auditAware")
+@OpenAPIDefinition(info = @Info(
+		title = "Accounts MicroService Rest Api Doc",
+		version = "v1",
+		contact = @Contact(
+				url = "https://www.linkedin.com/in/mohammad-mustafa1/"
+		)
+))
 public class AccountsApplication {
 
 	public static void main(String[] args) {
